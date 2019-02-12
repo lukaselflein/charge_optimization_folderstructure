c3709f33fddab9d43533dc2d5160933548667b48
lukaselflein <elfleinl@cs.uni-freiburg.de>
2019-02-12 17:19:41 +0100

    start of UA AA conversion - correct user

---
+++ bin/convert_UA_to_AA.py
@@ -0,0 +1,15 @@
+""" Converts United Atoms GROMACS structure file to All Atoms
+Copyright 2019 Simulation Lab
+University of Freiburg
+Author: Lukas Elflein <elfleinl@cs.uni-freiburg.de>
+"""
+
+
+def main():
+    """
+    Run the script.
+    """
+
+
+if __name__ == '__main__':
+    main()
