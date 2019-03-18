962bd38b9ffd763cbf0bdbec9bc2b40c3113933b
lukaselflein <elfleinl@cs.uni-freiburg.de>
2019-03-18 14:39:23 +0100

    +bader plotting; sum over residue; merge dataframes

--- bin/plot_charges.py
+++ bin/plot_charges.py
@@ -29,7 +29,7 @@ def extract_init_charges(rtp_path, df):
 
                 current_residuum = None
                 for line in rtp_text:
-                        # Atom names are only unique inside one residuum
+                        # atom names are only unique inside one residuum
                         # Thus, specify which res we are currently in
                         for residuum in residuum_names:
                                 if residuum in line:
@@ -85,8 +85,8 @@ def scatterplot_comparison(df, out_path, x_name='q', y_name='q_init'):
 	title = 'Comparison before/after self-consistent first cycle'	
 	fgrid.fig.suptitle(title)
 	fgrid.fig.subplots_adjust(top=0.9)  # Without this, the title is cut off
-	plt.ylabel('Charges before optimization')
-	plt.xlabel('Charges from Horton')
+	plt.ylabel('charges before optimization')
+	plt.xlabel('charges from Horton')
 	plt.savefig(os.path.join(out_path, 'scatter_before_after.png'))
 	print('Successfully plotted: {}'.format(title))
 	plt.clf()
@@ -97,13 +97,56 @@ def scatterplot_constraints(df, out_path, x_name='q', y_name='q_unconstrained'):
 	title = 'Effect of constraints on charges'
 	fgrid.fig.suptitle(title)
 	fgrid.fig.subplots_adjust(top=0.9)  # Without this, the title is cut off
-	plt.ylabel('Unconstrained Charges')
-	plt.xlabel('Constrained Charges q')
+	plt.ylabel('Unconstrained charges')
+	plt.xlabel('Constrained charges q')
 	plt.savefig(os.path.join(out_path, 'scatter_q_unconstrained.png'))
 	print('Successfully plotted: {}'.format(title))
 	plt.clf()
 
-def collect_charges():
+def collect_bader():
+	"""Find charges and put them in one dataframe."""
+	# Initialize collection data frame
+	coll_df = None
+	# Crawl the directory structure
+	for subdir, dirs, files in sorted(os.walk('./')):
+
+		# Exclude template folders from search
+		if 'template' in subdir or 'exclude' in subdir:
+			continue
+
+		# Select the folders with cost function
+		if 'bader_charges' in subdir:
+			print('Moving to {}'.format(subdir))
+			# Extract timestamp
+			time = os.path.split(subdir)[0].replace('./', '').replace('_ps_snapshot', '')
+			time = int(time)
+		
+			# Use the first charge file to come across as a template	
+			df = pd.read_csv(os.path.join(subdir, 'bader_charges.csv'), sep=r',\s*',
+					 engine='python')
+			df['timestamp'] = time
+
+			if coll_df is None:
+				coll_df = df
+			else:
+				coll_df = coll_df.append(df)
+
+	# The table still contains redundant hydrogen atoms: 1CD3... 2CB3
+	# Delete everything containing '1C' or '2C'
+	print(coll_df[coll_df.atom.str.contains(r'[1-2]C')])
+	coll_df = coll_df.drop(coll_df[coll_df.atom.str.contains(r'[1-2]C')].index)
+
+	print('All collected. Transforming wide to long format ...')
+	# Transform the wide format into a long format version (for easier plotting)
+	coll_df = coll_df.rename({'q': 'bader'}, axis=1)
+	coll_df = pd.melt(coll_df, id_vars=['atom', 'residue', 'timestamp'], 
+			  value_vars=['bader'])
+	coll_df = coll_df.rename({'value': 'charge', 'variable': 'Calculation Variant'},
+				 axis=1)
+	return coll_df
+
+
+def collect_horton():
 	"""Find charges and put them in one dataframe."""
 	# Initialize collection data frame
 	coll_df = None
@@ -132,11 +175,12 @@ def collect_charges():
 
 	print('All collected. Transforming wide to long format ...')
 	# Transform the wide format into a long format version (for easier plotting)
+	coll_df = coll_df.rename({'q': 'constrained', 'q_unconstrained': 'unconstrained'}, axis=1)
 	coll_df = pd.melt(coll_df, id_vars=['atom', 'residue', 'timestamp'], 
-			  value_vars=['q', 'q_unconstrained'])
-	coll_df = coll_df.rename({'value': 'Charge', 'variable': 'Constraint Type', 'atom': 'Atom'},
-				 axis=1)
+			  value_vars=['constrained', 'unconstrained'])
+	coll_df = coll_df.rename({'value': 'charge', 'variable': 'Calculation Variant'}, axis=1)
 	return coll_df
+
 
 def pointplot_errorbars(df, avg_df, out_path):
 	"""Plot stripplot with errorbars"""
@@ -147,15 +191,15 @@ def pointplot_errorbars(df, avg_df, out_path):
 	residue = df.residue.unique()
 
 	# Constrained
-	c_df = df.loc[df['Constraint Type'] == 'q']
-	pp = sns.pointplot('Charge', 'Atom', data=c_df, scale=1.2, 
+	c_df = df.loc[df['Calculation Variant'] == 'q']
+	pp = sns.pointplot('charge', 'atom', data=c_df, scale=1.2, 
 			     join=False, color='black',
 			     label='Constrained', zorder=100,
 			     err_style="std")
 	# unconstrained
 	sns.set_palette('pastel')
-	uc = df.loc[df['Constraint Type'] == 'q_unconstrained']
-	sns.pointplot('Charge', 'Atom', hue='residue', data=uc, alpha=0.3,
+	uc = df.loc[df['Calculation Variant'] == 'q_unconstrained']
+	sns.pointplot('charge', 'atom', hue='residue', data=uc, alpha=0.3,
 		      join=False, err_style='bars', dodge=0.2, zorder=1, ax=pp.axes, 
 		      scale = 0.75, type='bar', notch=True)
 
@@ -165,24 +209,8 @@ def pointplot_errorbars(df, avg_df, out_path):
 	pp.figure.savefig(os.path.join(out_path, 'pointplot_confidence.png'))
 	plt.clf()
 
-def boxplot(df, avg_df, out_path):
-	""" Boxplot."""
-	fig = plt.figure(figsize=(16,10))
-
-
-	# Unconstrained
-	sns.set_palette('pastel')
-	uc = df.loc[df['Constraint Type'] == 'q_unconstrained']
-	bp = sns.boxplot(x='Charge', y='Atom', hue='residue', data=uc, whis=100)
-	ax = bp.axes
-
-	# Constrained
-	c_df = df.loc[df['Constraint Type'] == 'q']
-	sns.boxplot(x='Charge', y='Atom', data=c_df, color='black', ax=ax, whis=100)
-
-	# Averaged cost function
-	sns.pointplot('q', 'atom', data=avg_df, ax=ax, join=False, color='firebrick')
-
+def make_edgecolor(ax):
+	"""Make boxes transparent with colored edges & whiskers"""
 	for i,artist in enumerate(ax.artists):
 		# Set the linecolor on the artist to the facecolor, and set the facecolor to None
 		col = artist.get_facecolor()
@@ -197,17 +225,36 @@ def boxplot(df, avg_df, out_path):
 			line.set_mfc(col)
 			line.set_mec(col)
 
-	handles, _ = ax.get_legend_handles_labels()
-	# Complete the legend by adding constraint condition to the labels
-	residue = ['Unconstrained ' + r for r in df.residue.unique()]
-	# Second plot does not show up in handles, unclear how to add it to legend
-	ax.legend(handles, [residue[0], residue[1], residue[2]])  # , 'Constrained', 'Averaged Cost'])
-	
+def boxplot(df, out_path):
+	""" Boxplot."""
+	fig = plt.figure(figsize=(16,10))
+
+	# Individual horton charges
+	sns.set_palette(sns.color_palette("Set1", n_colors=4, desat=.9))
+	bp = sns.boxplot(x='charge', y='atom', hue='Calculation Variant', data=df, whis=100)
+	ax = bp.axes
+
+	split_residues = False	
+	if split_residues:
+		# Unconstrained
+		sns.set_palette('pastel')
+		uc = df.loc[df['Calculation Variant'] == 'q_unconstrained']
+		bp = sns.boxplot(x='charge', y='atom', hue='residue', data=uc, whis=100)
+
+		# Constrained
+		c_df = df.loc[df['Calculation Variant'] == 'q']
+		sns.boxplot(x='charge', y='atom', data=c_df, color='black', ax=ax, whis=100)
+
+		# Averaged cost function
+		sns.pointplot('q', 'atom', data=avg_df, ax=ax, join=False, color='firebrick')
+
+	make_edgecolor(ax)
 	ax.yaxis.grid(True)  # Show horizontal gridlines
 	ax.tick_params(grid_alpha=0.5)
 	ax.set_xlim(-2, 2)
 	bp.figure.savefig(os.path.join(out_path, 'boxplot.png'))
 	plt.clf()
+
 
 def main():
 	"""Execute everything. """
@@ -216,21 +263,38 @@ def main():
 	# Setup the directory structure
 	out_path = create_dir()
 
-	# Collect all individual charges
-	print('Collecting charges ...')
-	collect_df = collect_charges()
-
 	# Read charges from averaged cost function
 	input_path = './horton_charges/fitted_point_charges.csv'
 	avg_df = pd.read_csv(input_path)
+	avg_df = avg_df.rename({'q': 'averaged cost function'}, axis=1)
+	avg_df = pd.melt(avg_df, id_vars=['atom', 'residue'], value_vars=['averaged cost function'])
+	avg_df = avg_df.rename({'value': 'charge', 'variable': 'Calculation Variant'}, axis=1)
 
-	# Pointplot charges with errorbars to get confidence intervals
-	pointplot_errorbars(collect_df, avg_df, out_path=out_path)
-	print('Pointplot saved.')
+	# Collect all horton charges
+	print('Collecting HORTON charges ...')
+	horton_df = collect_horton()
+
+	# Collect all bader charges
+	print('Collecting Bader charges ...')
+	bader_df = collect_bader()
+
+	# Paste everything into single dataframe
+	constr_df = horton_df.loc[horton_df['Calculation Variant'] == 'constrained']
+	unconstr_df = horton_df.loc[horton_df['Calculation Variant'] == 'unconstrained']
+	collect_df = avg_df
+	collect_df = collect_df.append(constr_df, sort=False) 
+	collect_df = collect_df.append(unconstr_df, sort=False) 
+	collect_df = collect_df.append(bader_df, sort=False)
+
+	collect_df.to_csv('test.csv')
 
 	# Boxplot charges to get min/max errorbars
-	boxplot(collect_df, avg_df, out_path=out_path)
+	boxplot(collect_df, out_path=out_path)
 	print('Boxplot saved.')
+
+	# Pointplot charges with errorbars to get confidence intervals
+	# pointplot_errorbars(collect_df, avg_df, out_path=out_path)
+	# print('Pointplot saved.')
 
 	# Plot constrained vs unconstrained charges
 	# scatterplot_constraints(collect_df, out_path)
@@ -240,9 +304,9 @@ def main():
 	# Put both old and new charges in same table for comparison
 	merged_df = old_charges.merge(avg_df, how='inner', on=['atom', 'residue'])
 	# Throw away unneccessary information
-	merged_df = merged_df[['residue', 'atom', 'q_init', 'q']]
+	# merged_df = merged_df[['residue', 'atom', 'q_init', 'q']]
 	# Plot the old and new charges 
-	scatterplot_comparison(merged_df, out_path)
+	# scatterplot_comparison(merged_df, out_path)
 
 	# merged_df.to_csv(os.path.join(out_path, 'charge_comparison.csv'))
 	print('Done.')
