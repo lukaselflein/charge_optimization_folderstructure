bb26b5a6ec5570357d3d779d8e0728708f73889a
lukaselflein <elfleinl@cs.uni-freiburg.de>
2019-03-28 15:25:59 +0100

    start packaging as module

---
+++ bin/__init__.py
@@ -0,0 +1,16 @@
+"""Charge Optimization Workflow module."""
+
+from . import aa2ua_cube
+from . import loop_charges
+from . import average_cost                      
+from . import loop_convert_AA_to_UA
+from . import charges_to_rtp                    
+from . import loop_convert_UA_to_AA
+from . import convert_UA_to_AA                  
+from . import loop_cost_functions
+from . import create_snapshots_from_trajectory  
+from . import loop_submit
+from . import fitESPconstrained                 
+from . import plot_charges
+from . import gpaw_optimize_and_esp             
+from . import loop_bader
