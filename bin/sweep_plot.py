d50e311201281147009cfbd8fa1a004d9147a0f6
lukaselflein <elfleinl@cs.uni-freiburg.de>
2019-06-14 13:29:10 +0200

    plot variance of snapshots in pointplot

--- bin/sweep_plot.py
+++ bin/sweep_plot.py
@@ -5,6 +5,7 @@ Author: Lukas Elflein <elfleinl@cs.uni-freiburg.de>
 """
 
 import os
+import re
 import sys
 import shutil
 import pandas as pd
@@ -12,19 +13,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 from smamp.tools import find
-
-def collect_average():
-	"""Put averaged charges in a dataframe."""
-	# Read charges from averaged cost function
-	input_path = './horton_charges/fitted_point_charges.csv'
-	avg_df = pd.read_csv(input_path)
-	# Rename columns for consistency
-	avg_df = avg_df.rename({'q': 'averaged cost function'}, axis=1)
-	# Transform to long format
-	avg_df = pd.melt(avg_df, id_vars=['atom', 'residue'], value_vars=['averaged cost function'])
-	avg_df = avg_df.rename({'value': 'charge', 'variable': 'Calculation Variant'}, axis=1)
-	return avg_df
-
 
 def default_style(func):
 	"""A decorator for setting global plotting styling options."""
@@ -38,42 +26,97 @@ def default_style(func):
 	return wrapper
 
 
-@default_style
-def pointplot(df, out_path, variant='constrained'):
-	"""Pointplot with different colors for every snapshot"""
-	c_df = df.loc[df['Calculation Variant'] == variant]
-	pp = sns.pointplot('charge', 'atom', data=c_df, scale=1.0, 
-			     join=False, hue='timestamp', ci='sd', dodge=0.1)
-
-	pp.set_title('{} charges of all residues'.format(variant).capitalize())
-	pp.axes.grid(True)  # Show horizontal gridlines
-	pp.figure.savefig(os.path.join(out_path, 'pointplot_{}.png'.format(variant)))
-
-@default_style
-def main():
-	"""Execute everything. """
-	print('This is {}.'.format(__file__))
-
-	print('Collecting ...')
-
+def collect_averages():
 	collect_df = pd.DataFrame()
 	for lnrho in range(-10, 0):
 		charge_file = find(path='.', folder_keyword='horton_charges/sweep_rhoref', 
-				  file_keyword='avg_charges_{}.csv'.format(lnrho), 
+				  file_keyword='charges_{}.csv'.format(lnrho), 
 				  nr_occ=1)[0]
 		df = pd.read_csv(charge_file)
 		df['lnrho'] = lnrho
 		collect_df = collect_df.append(df)
 
 	collect_df = pd.melt(collect_df, id_vars=['atom', 'residue', 'lnrho'], value_vars=['q'])
-	print(collect_df)
+	return collect_df
 
-	print('Plotting ... ')
-	pp = sns.pointplot('value', 'atom', data=collect_df, scale=1.0, 
+
+@default_style
+def plot_averages(df):
+	pp = sns.pointplot('value', 'atom', data=df, scale=1.0, 
 		 	    join=False, hue='lnrho', ci='sd', dodge=0.1, 
 			    palette=sns.color_palette("coolwarm", 10))
 
-	pp.figure.savefig(os.path.join('pointplot.png'))
+	pp.set_title('Averaged-Costfunction charges of all residues')
+	pp.axes.grid(True)  # Show horizontal gridlines
+	pp.figure.savefig('pointplot.png')
+
+
+
+def collect_snapshots(plot_range=[-9, -5]):
+	collect_df = pd.DataFrame()
+	for lnrho in plot_range: #range(-10, 0):
+		cost_paths = find(path='.', folder_keyword='4_horton_cost_function/lnrho_sweep', 
+				  file_keyword='charges_{}.csv'.format(lnrho), 
+				  nr_occ=1)
+		for charge_file in cost_paths:
+			df = pd.read_csv(charge_file)
+
+			# Paste the lnrho parameter into the dataframe
+			df['lnrho'] = lnrho
+
+			# Also note the snapshot identifier
+			timestamp = re.findall(r'\d+', charge_file)[0]
+			df['snapshot'] = timestamp
+
+			collect_df = collect_df.append(df)
+
+	collect_df = pd.melt(collect_df, id_vars=['atom', 'residue', 'lnrho', 'snapshot'], 
+			     value_vars=['q'])
+	return collect_df
+
+
+@default_style
+def plot_snapshots(df):
+	pp = sns.pointplot('value', 'atom', data=df, scale=1.0, 
+		 	    join=False, hue='lnrho', ci='sd', dodge=0.1, 
+			    palette=sns.color_palette("coolwarm", 10))
+
+	pp.set_title('Individual snapshot charges of all residues')
+	pp.axes.grid(True)  # Show horizontal gridlines
+	pp.figure.savefig('snapshots_pointplot.png')
+
+
+@default_style
+def plot_joint(avg_df, snap_df):
+	pp = sns.pointplot('value', 'atom', data=snap_df, scale=0.8, 
+		 	    join=False, hue='lnrho', ci='sd', dodge=0.1, 
+			    palette=sns.color_palette("coolwarm", 10))
+	pp = sns.pointplot('value', 'atom', data=avg_df, scale=1.0, 
+		 	    join=False, hue='lnrho', ci='sd', dodge=0.1, 
+			    palette=sns.color_palette("YlGn", 10),
+			    markers='+')
+
+	pp.set_title('Charges of all residues')
+	pp.axes.grid(True)  # Show horizontal gridlines
+	pp.figure.savefig('joint_pointplot.png')
+
+
+def main():
+	"""Execute everything. """
+	print('This is {}.'.format(__file__))
+
+	# Averages
+	print('Collecting averages ...')
+	average_df = collect_averages()
+	print('Plotting averages ...')
+	plot_averages(average_df)
+
+	# Individual snapshots
+	print('Collecting snapshots ...')
+	snapshot_df = collect_snapshots()
+	print('Plotting snapshots ...')
+	plot_snapshots(snapshot_df)
+	plot_joint(average_df, snapshot_df)
 
 	print('Done.')
 
