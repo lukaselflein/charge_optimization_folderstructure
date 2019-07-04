b17a5ced4ffc740dc0e246ffcf004ac173ae61bd
lukaselflein <elfleinl@cs.uni-freiburg.de>
2019-07-04 11:19:06 +0200

    change sweep volume

--- bin/sweep_2d_plot.py
+++ bin/sweep_2d_plot.py
@@ -17,98 +17,101 @@ from smamp.tools import find
 
 
 def collect_snapshots(plot_range=[-9, -5]):
-	collect_df = pd.DataFrame()
-	cost_paths = find(path='.', folder_keyword='4_horton_cost_function/lnrho_sweep', 
-			  file_keyword='charge',
-			  nr_occ=None)
+   collect_df = pd.DataFrame()
+   cost_paths = find(path='.', folder_keyword='4_horton_cost_function/lnrho_sweep', 
+           file_keyword='charge',
+           nr_occ=None)
 
-	for charge_file in cost_paths:
-		# Parse parameters from filename
-		lnrho, sigma = charge_file[-15:-4].split('_')[-2:]
+   for charge_file in cost_paths:
+      # Parse parameters from filename
+      lnrho, sigma = charge_file[-15:-4].split('_')[-2:]
 
-		# Read file
-		df = pd.read_csv(charge_file)
+      # Read file
+      df = pd.read_csv(charge_file)
 
-		# Paste the lnrho parameter into the dataframe
-		df['lnrhoref'] = int(lnrho)
-		df['sigma'] = float(sigma)
+      # Paste the lnrho parameter into the dataframe
+      df['lnrhoref'] = int(lnrho)
+      df['sigma'] = float(sigma)
 
-		# Also note the snapshot identifier
-		timestamp = re.findall(r'\d+', charge_file)[0]
-		df['snapshot'] = timestamp
-		# df['diff'] = (df.q - df.q_unconstrained).abs()
-		df['diff'] = (df.q - df.q_unconstrained)**2
+      # Also note the snapshot identifier
+      timestamp = re.findall(r'\d+', charge_file)[0]
+      df['snapshot'] = timestamp
+      # df['diff'] = (df.q - df.q_unconstrained).abs()
+      df['diff'] = (df.q - df.q_unconstrained)**2
 
-		collect_df = collect_df.append(df)
+      collect_df = collect_df.append(df)
 
-	collect_df = pd.melt(collect_df, id_vars=['atom', 'residue', 'snapshot', 'lnrhoref', 'sigma'], 
-			     value_vars=['diff'])
-	return collect_df
+   collect_df = pd.melt(collect_df, id_vars=['atom', 'residue', 'snapshot', 'lnrhoref', 'sigma'], 
+              value_vars=['diff'])
+   return collect_df
 
 
 def collect_avg():
-	collect_df = pd.DataFrame()
-	cost_paths = find(path='.', folder_keyword='horton_charges/sweep_rhoref',
-			  file_keyword='charges',
-			  nr_occ=None)
+   collect_df = pd.DataFrame()
+   cost_paths = find(path='.', folder_keyword='horton_charges/sweep_rhoref',
+           file_keyword='charges',
+           nr_occ=None)
 
-	for charge_file in cost_paths:
-		#print(charge_file)
-		# Parse parameters from filename
-		lnrho, sigma = charge_file[-15:-4].split('_')[-2:]
-		#print(lnrho, sigma)
+   for charge_file in cost_paths:
+      #print(charge_file)
+      # Parse parameters from filename
+      lnrho, sigma = charge_file[-15:-4].split('_')[-2:]
+      sarah_wish_sigma = [0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4]
+      sigma = [s for s in sigma if s in sarah_wish_sigma]
 
-		# Read file
-		df = pd.read_csv(charge_file)
+      #print(lnrho, sigma)
 
-		# Paste the lnrho parameter into the dataframe
-		df['lnrhoref'] = int(lnrho)
-		df['sigma'] = float(sigma)
+      # Read file
+      df = pd.read_csv(charge_file)
 
-		# Also note the snapshot identifier
-		df['diff'] = (df.q - df.q_unconstrained)**2
+      # Paste the lnrho parameter into the dataframe
+      df['lnrhoref'] = int(lnrho)
+      df['sigma'] = float(sigma)
 
-		collect_df = collect_df.append(df)
+      # Also note the snapshot identifier
+      df['diff'] = (df.q - df.q_unconstrained)**2
 
-	collect_df = pd.melt(collect_df, id_vars=['atom', 'residue', 'lnrhoref', 'sigma'], 
-			     value_vars=['diff'])
-	return collect_df
+      collect_df = collect_df.append(df)
+
+   collect_df = pd.melt(collect_df, id_vars=['atom', 'residue', 'lnrhoref', 'sigma'], 
+              value_vars=['diff'])
+   return collect_df
 
 def molten_to_2d(df):
-	df = df.groupby(['lnrhoref', 'sigma']).mean().apply(np.sqrt)
-	df = df.unstack('sigma')
-	df.columns = df.columns.droplevel()
-	df = df.transpose()
-	df = df.sort_index(ascending=False)
-	return df
+   df = df.groupby(['lnrhoref', 'sigma']).mean().apply(np.sqrt)
+   df = df.unstack('sigma')
+   df.columns = df.columns.droplevel()
+   df = df.transpose()
+   df = df.sort_index(ascending=False)
+   return df
 
 def plot_heatmap(df, outname):
-	fig = plt.figure(figsize=(16,10))
-	sns.set_context("talk", font_scale=0.9)
-	pp = sns.heatmap(data=df, cmap='coolwarm') 
-	pp.set_title('RRMSD between constrained and unconstrained charges [e].')
-	pp.figure.savefig(outname)
+   fig = plt.figure(figsize=(16,10))
+   sns.set_context("talk", font_scale=0.9)
+   pp = sns.heatmap(data=df, cmap='coolwarm') 
+   pp.set_title('RRMSD between constrained and unconstrained charges [e].')
+   pp.figure.savefig(outname)
 
 def main():
-	"""Execute everything. """
-	print('This is {}.'.format(__file__))
+   """Execute everything. """
+   print('This is {}.'.format(__file__))
 
 
-	print('Collecting averages')
-	df = collect_avg()
-	df = molten_to_2d(df)
-	plot_heatmap(df, outname='avg_heatmap.png')
-	print(df)	
+   print('Collecting averages')
+   df = collect_avg()
+   df = molten_to_2d(df)
+   plot_heatmap(df, outname='avg_heatmap.png')
+   print(df)   
 
-	# Individual snapshots
-	print('Collecting snapshots ...')
-	df = collect_snapshots(plot_range=range(-9, 0))
-	df = molten_to_2d(df)
-	plot_heatmap(df, outname='snapshot_heatmap.png')
-	print(df)	
+   # Individual snapshots
+   print('Collecting snapshots ...')
+   df = collect_snapshots(plot_range=range(-9, 0))
+   df = molten_to_2d(df)
+   plot_heatmap(df, outname='snapshot_heatmap.png')
+   print(df)   
 
-	print('Done.')
+   print('Done.')
 
 
 if __name__ == '__main__':
-	main()
+   main()
--- bin/sweep_rhoref.py
+++ bin/sweep_rhoref.py
@@ -16,72 +16,72 @@ from loop_cost_functions import calc_cost_function
 from smamp.tools import cd
 from smamp.tools import check_existence
 def testprint(*args, **kwargs):
-	return 'args: {}, kwargs: {}'.format(args, kwargs)
+   return 'args: {}, kwargs: {}'.format(args, kwargs)
 
 def get_tasks(path_to_subdir):
-	"""Vary the lnrho weighting parameter, create folder and execute."""
-	sweep_dir = 'lnrho_sweep'
-	if os.path.exists(sweep_dir):
-		# print('Removing old dir')
-		# shutil.rmtree(sweep_dir)
-		pass
-	else:
-		print('making dir')
-		os.mkdir(sweep_dir)
-		print('dir made.')
+   """Vary the lnrho weighting parameter, create folder and execute."""
+   sweep_dir = 'lnrho_sweep'
+   if os.path.exists(sweep_dir):
+      # print('Removing old dir')
+      # shutil.rmtree(sweep_dir)
+      pass
+   else:
+      print('making dir')
+      os.mkdir(sweep_dir)
+      print('dir made.')
 
-	tasks = []
-	#for sigma in [0.6, 0.8, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5]:
-	for sigma in [0.6, 0.8, 1.0, 1.2, 1.4]:
-		for lnrho in [-9, -8, -7, -6, -5, -4, -3, -2]:
-			output_name = os.path.join(sweep_dir, 'cost_{}_{}.h5'.format(lnrho, sigma))
-			skipped = 0
-			if os.path.exists(output_name):
-				# print('{} exists. Do not include in worklist.'.format(output_name))
-				skipped += 1
-				continue
-			
-			else:
-				tasks += [(path_to_subdir, lnrho, sigma, output_name)]
-	print('{} files found and skipped.'.format(skipped))
-	return tasks
+   tasks = []
+   #for sigma in [0.6, 0.8, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5]:
+   for sigma in [0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4]:
+      for lnrho in [-9, -8, -7, -6, -5, -4, -3, -2]:
+         output_name = os.path.join(sweep_dir, 'cost_{}_{}.h5'.format(lnrho, sigma))
+         skipped = 0
+         if os.path.exists(output_name):
+            # print('{} exists. Do not include in worklist.'.format(output_name))
+            skipped += 1
+            continue
+         
+         else:
+            tasks += [(path_to_subdir, lnrho, sigma, output_name)]
+   print('{} files found and skipped.'.format(skipped))
+   return tasks
 
 def calculate_tasks(tasks):
-	print('{} items in worklist.'.format(len(tasks)))
-	random.shuffle(tasks)
-	for task in tasks:
-		if os.path.exists(task[-1]):
-			#print('{} exists. Skipping ahead.'.format(task[-1]))
-			continue
+   print('{} items in worklist.'.format(len(tasks)))
+   random.shuffle(tasks)
+   for task in tasks:
+      if os.path.exists(task[-1]):
+         #print('{} exists. Skipping ahead.'.format(task[-1]))
+         continue
 
-		print('starting {} {} in {}'.format(task[1], task[2], task[0]))
-		with cd(task[0]):
-			calc_cost_function(*task)
-		
+      print('starting {} {} in {}'.format(task[1], task[2], task[0]))
+      with cd(task[0]):
+         calc_cost_function(*task)
+      
 
 def main():
-	""" Execute everything."""
-	print('This is {}.'.format(__file__))
-	print('Current working dir: {}'.format(os.getcwd()))
-	
-	tasks = []
+   """ Execute everything."""
+   print('This is {}.'.format(__file__))
+   print('Current working dir: {}'.format(os.getcwd()))
+   
+   tasks = []
 
-	# Crawl the directory structure
-	for subdir, dirs, files in sorted(os.walk('.')):
+   # Crawl the directory structure
+   for subdir, dirs, files in sorted(os.walk('.')):
 
-		# Exclude template folders from search
-		if 'template' in subdir or 'exclude' in subdir or 'lnrho_sweep' in subdir:
-			continue
+      # Exclude template folders from search
+      if 'template' in subdir or 'exclude' in subdir or 'lnrho_sweep' in subdir:
+         continue
 
-		# Select the folder to calculate in
-		if 'horton_cost_function' in subdir:
-			print('Moving to {}'.format(subdir))
-			with cd(subdir):
-				subdir_tasks = get_tasks(subdir)
-				tasks += subdir_tasks
-	
-	calculate_tasks(tasks)
-	print('Done.')
+      # Select the folder to calculate in
+      if 'horton_cost_function' in subdir:
+         print('Moving to {}'.format(subdir))
+         with cd(subdir):
+            subdir_tasks = get_tasks(subdir)
+            tasks += subdir_tasks
+   
+   calculate_tasks(tasks)
+   print('Done.')
 
 if __name__ == '__main__':
-	main()
+   main()
