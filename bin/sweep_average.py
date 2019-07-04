2e4424b58535b68c75e359b41cefbb35fc6a2439
lukaselflein <elfleinl@cs.uni-freiburg.de>
2019-07-04 14:41:15 +0200

    skip non-existing cost functions

--- bin/sweep_average.py
+++ bin/sweep_average.py
@@ -15,54 +15,58 @@ from average_cost import read_h5, collect_matrices, average, export
 
 
 def main():
-	""" Run the script."""
-	print('This is {}.'.format(__file__))
-	# Create a folder for the averaged cost function
-	chargepath = './horton_charges/sweep_rhoref'
-	if os.path.isdir(chargepath):
-		pass
-		# shutil.rmtree(chargepath)
-	else:
-		os.makedirs(chargepath)
+   """ Run the script."""
+   print('This is {}.'.format(__file__))
+   # Create a folder for the averaged cost function
+   chargepath = './horton_charges/sweep_rhoref'
+   if os.path.isdir(chargepath):
+      pass
+      # shutil.rmtree(chargepath)
+   else:
+      os.makedirs(chargepath)
 
-	cost_paths = find(path='.', folder_keyword='4_horton_cost_function/lnrho_sweep', 
-			  file_keyword='cost',
-			  nr_occ=None)
-	lnrho_range = []
-	sigma_range = []
-	for charge_file in cost_paths:
-		# print(charge_file)
-		# Parse parameters from filename
-		lnrho, sigma = charge_file[-15:-3].split('_')[-2:]
-		lnrho_range += [lnrho]
-		sigma_range += [sigma]
+   cost_paths = find(path='.', folder_keyword='4_horton_cost_function/lnrho_sweep', 
+           file_keyword='cost',
+           nr_occ=None)
+   lnrho_range = []
+   sigma_range = []
+   for charge_file in cost_paths:
+      # print(charge_file)
+      # Parse parameters from filename
+      lnrho, sigma = charge_file[-15:-3].split('_')[-2:]
+      lnrho_range += [lnrho]
+      sigma_range += [sigma]
 
-	lnrho_range = set(lnrho_range)
-	sigma_range = set(sigma_range)
+   lnrho_range = set(lnrho_range)
+   sigma_range = set(sigma_range)
 
-	for lnrho in lnrho_range:
-		for sigma in sigma_range:
-			# print('lnrho = {}, sigma = {}'.format(lnrho, sigma))
-			filename = 'cost_{}_{}.h5'.format(lnrho, sigma)
-			cost_function_paths = find(path='.', folder_keyword='lnrho_sweep', 
-						   file_keyword=filename, nr_occ=None)
+   for lnrho in lnrho_range:
+      for sigma in sigma_range:
+         print('lnrho = {}, sigma = {}'.format(lnrho, sigma))
+         filename = 'cost_{}_{}.h5'.format(lnrho, sigma)
+         cost_function_paths = find(path='.', folder_keyword='lnrho_sweep', 
+                     file_keyword=filename, nr_occ=None)
 
-			# Extract cost function As and Bs
-			A_matrices, B_vectors = collect_matrices(cost_function_paths)
+         # print(cost_function_paths)
+         if len(cost_function_paths) == 0:
+            print('WARNING: No cost functions found! Continue ..')
+            continue
+         # Extract cost function As and Bs
+         A_matrices, B_vectors = collect_matrices(cost_function_paths)
 
-			# Average over all matrices & vectors
-			average_A, average_B = average(A_matrices, B_vectors)
+         # Average over all matrices & vectors
+         average_A, average_B = average(A_matrices, B_vectors)
 
-			# keep one HDF5 file as a template for writing into later
-			out_name =  'costfunction_average_{}_{}.h5'.format(lnrho, sigma)
-			template_path = os.path.join(chargepath, out_name) 
-			shutil.copyfile(cost_function_paths[0], template_path)
+         # keep one HDF5 file as a template for writing into later
+         out_name =  'costfunction_average_{}_{}.h5'.format(lnrho, sigma)
+         template_path = os.path.join(chargepath, out_name) 
+         shutil.copyfile(cost_function_paths[0], template_path)
 
-			# Export matrices to hdf5 
-			export(average_A, average_B, template_path=template_path)
+         # Export matrices to hdf5 
+         export(average_A, average_B, template_path=template_path)
 
-	print('Done.')
+   print('Done.')
 
 
 if __name__ == '__main__':
-	main()
+   main()
