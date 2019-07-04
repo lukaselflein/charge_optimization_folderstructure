fd0822a242cc0bda98dd309278f96b1d3134a79e
lukaselflein <elfleinl@cs.uni-freiburg.de>
2019-07-04 09:49:08 +0200

    change parameter sweep volume

--- bin/sweep_rhoref.py
+++ bin/sweep_rhoref.py
@@ -31,7 +31,8 @@ def get_tasks(path_to_subdir):
 		print('dir made.')
 
 	tasks = []
-	for sigma in [0.6, 0.8, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5]:
+	#for sigma in [0.6, 0.8, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5]:
+	for sigma in [0.6, 0.8, 1.0, 1.2, 1.4]:
 		for lnrho in [-9, -8, -7, -6, -5, -4, -3, -2]:
 			output_name = os.path.join(sweep_dir, 'cost_{}_{}.h5'.format(lnrho, sigma))
 			skipped = 0
